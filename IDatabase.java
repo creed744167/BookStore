7af27873336f6c77c49d2e3c450cbf3430e32837
creed744167 <42743078+creed744167@users.noreply.github.com>
2021-08-07 16:09:39 +0530

    Add files via upload

---
+++ IDatabase.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class IDatabase {
+
+	public static final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
+	public static final String CONNECTION_STRING = "jdbc:oracle:thin:@localhost:1521:xe";
+	public static final String USER_NAME = "hr";
+	public static final String PASSWORD = "hr";
+
+}
